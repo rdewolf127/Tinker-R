2b46b4da12e13d00f8009ac73f118ca5b492b6e3
Renee Dewolf <santoshamom@gmail.com>
2017-11-02 21:34:50 -0400

    Update

--- MySQL_Connection.R
+++ MySQL_Connection.R
@@ -4,10 +4,29 @@
 install.packages ('RMySQL',type='source')
 library(RMySQL)
 
-#connect to the db
+#connect to the server
 ucscDb <- dbConnect(MySQL(),user="genome", host="genome-mysql.cse.ucsc.edu")
 
+#show all dbs on the server
 result <- dbGetQuery(uscsDb, "show databases;")
 
-#disconnect from the db
+#disconnect from the server
 dbDisconnect(ucscDb)
+
+#connect to a specific db on the server
+hg19 <- dbConnect(MySQL(),user="genome", db="hg19", host="genome-mysql.cse.ucsc.edu")
+
+#list all tables in the db
+allTables <- dbListTables(hg19)
+
+#get the number of tables in the db
+length(allTables)
+
+#get list of all fields in a specific table
+dbListFields(hg19, "affyU133Plus2")
+
+#query the table
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+
+#create a df from extracted data
+affydata <- dbReadTable(hg19, "affyU133Plus2")
