193f1d6e7680ad23bdd320540a0547b7789ec6f2
Renee Dewolf <santoshamom@gmail.com>
2017-11-03 20:55:19 -0400

    Update

--- Read_From_API.R
+++ Read_From_API.R
@@ -4,10 +4,10 @@
 library(httr)
 
 myApp = oauth_app("twitter",
-                  key="nRB0tVZs22UUMA7eXkbqIMKeF", secret="ZE3EJIiJqS90ZxxJm2uCxr5m6ZhTTa8UdUh5BcBDxk21LTvIeC")
+                  key="keyfromapp", secret="secretfromapp")
 
 sig = sign_oauth1.0(myApp, token=
-                      "924528038-LgnJrRR260ye4mSsYPFpb9jUN4NSziRR6xwR9u9Y", token_secret="N3QoBJBbH8ElDqxlrU87w54MQrHucm7lhDfhWZ2iCrJiR")
+                      "tokenfromapp", token_secret="secretfromapp")
 
 homeTL = GET("https://api.twitter.com/1.1/statuses/home_timeline.json", sig)
 
