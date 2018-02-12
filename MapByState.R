8317d9deac8511424dbf9b5fe486141e9523c41b
Renee Dewolf <santoshamom@gmail.com>
2018-02-12 11:38:30 -0500

    Initial Commit

---
+++ MapByState.R
@@ -0,0 +1,26 @@
+#install.packages('ggmap')
+library(ggmap)
+
+#load in your data that is summarized by state
+mydata <- read.csv('c:/users/renee/desktop/srasbystate.csv', colClasses = c("character", "numeric"))
+head(mydata)
+
+#load in the state lat/lon file
+statelatlon <- read.csv('c:/users/renee/desktop/statelatlon.csv', colClasses = c("character", "numeric", "numeric"))
+head(statelatlon)
+
+mydata <- merge(mydata, statelatlon, by="State")
+head(mydata)
+
+#drop Hawaii and Alaska to tighten the map 
+mydata = mydata[mydata$State != "Alaska",]
+mydata = mydata[mydata$State != "Hawaii",]
+
+#get the center of the US from Google Maps
+usa_center = as.numeric(geocode("United States"))
+USAMap = ggmap(get_googlemap(center=usa_center, scale = 2, zoom =4), extent = "normal")
+
+#plot it
+USAMap+
+  geom_point(aes(x=Longitude, y=Latitude), data=mydata, col="purple", alpha=0.4,
+             size=mydata$SRARatio) 
